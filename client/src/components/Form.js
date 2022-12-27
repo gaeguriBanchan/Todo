dd61f8c4c7742a3c6a79da953d26337fe30fd0d4
mochapot <rlawowls93@gmail.com>
2022-12-27 17:29:04 +0900

    function 형식으로 변경, tailwindcss 적용

---
+++ client/src/components/Form.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Form = ({ todoValue, setTodoValue, addTodoSubmit }) => {
+  const changeTodoValue = (event) => {
+    // console.log(event.target.value);
+    setTodoValue(event.target.value);
+  };
+  return (
+    <div>
+      <form onSubmit={addTodoSubmit} className="flex pt-2">
+        <input
+          type="text"
+          placeholder="할 일을 입력하시오."
+          className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
+          value={todoValue}
+          onChange={changeTodoValue}
+        />
+        <input
+          type="submit"
+          className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white hover:bg-blue-400"
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Form;
